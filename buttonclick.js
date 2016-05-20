aafb03ab6a4067a352378aa31d671782d47b1168
Neil Clayton <ncairrifle@outlook.com>
2016-05-20 21:46:00 +0100

    unused

--- buttonclick.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('#faqbutton1').click(function(){
-        $child=$(this).children('button');
-        $child.toggleClass("glyphicon-chevron-down").toggleClass("glyphicon-chevron-up");
-    });
-});
