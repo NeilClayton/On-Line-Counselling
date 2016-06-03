9b3f17d37e51fdb42c4d7afbc6aa7fe9d01cac7b
Dominic Hart <dom_h@live.co.uk>
2016-06-03 18:57:19 +0100

    Styling success page
    Adding styles to the success page

--- success.php
+++ success.php
@@ -1,7 +1,7 @@
 <?php
-include('include/function.php');
+/*include('include/function.php');
 include('include/connection.php');
-echo connection_open();
+echo connection_open();*/
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -23,67 +23,77 @@ echo connection_open();
 	<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
-    <body>
+    <body id="success">
         <?php
-        if (!empty($_REQUEST)) {
-            $product_no = $_REQUEST['item_number']; 
-            $product_transaction = $_REQUEST['tx']; 
-            $product_price = $_REQUEST['amt']; 
-            $product_currency = $_REQUEST['cc']; 
-            $product_status = $_REQUEST['st']; 
-            $product_no = base64_decode($product_no);
-            $data = array
-			(
-                'product_id' => $product_no
-            );
-            $query = Select_Record_By_One_Filter($data, 'tbl_product_detail');
-            $query->setFetchMode(PDO::FETCH_ASSOC);
-            $result = $query->fetch();
-            echo connection_close();
-        }
+        // if (!empty($_REQUEST)) {
+            // $product_no = $_REQUEST['item_number']; 
+            // $product_transaction = $_REQUEST['tx']; 
+            // $product_price = $_REQUEST['amt']; 
+            // $product_currency = $_REQUEST['cc']; 
+            // $product_status = $_REQUEST['st']; 
+            // $product_no = base64_decode($product_no);
+            // $data = array
+			// (
+                // 'product_id' => $product_no
+            // );
+            // $query = Select_Record_By_One_Filter($data, 'tbl_product_detail');
+            // $query->setFetchMode(PDO::FETCH_ASSOC);
+            // $result = $query->fetch();
+            // echo connection_close();
+        // }
         ?>
 		<section class="container-fluid">
 			<section class="row success">
-				<section class="col-md-8 col-md-offset-2">
-					<h1>Successful Transaction</h1>
-					<h2>Payment Status</h2>
-					<section class="receipt">
-					
-					<!--Successful Transaction -->
-					<h3>Payment Successful</h3>
-					<P>Transaction Status - Complete </P>
-					<P>Transaction Id - 5GHTJIDM5$S5 </P>
-					<P><a href='fees.php'>Back to Fees page</a></P>
-					<br>
-					<br>
-					<br>
-					<br>
-					<!--Failed Transaction -->
-					<h3>Payment Failed</h3>
-					<P>Transaction Status - Failed</P>
-					<P>Transaction Id - N/A </P>
-					<P><a href='fees.php'>Back to Fees page</a></P>					
-					
-					
-                <?php
-                #Rechecking the product price and currency detail
-				
-               # if ($product_price == $result['product_price'] && $product_currency == $result['product_currency']) 
-				#{
-                    #echo "<h3>Payment Successful</h3>";
-                    #echo "<P>Transaction Status - " . $product_status . "</P>";
-                    #echo "<P>Transaction Id - " . $product_transaction . "</P>";
-                    #echo "<p><a href='fees.php'>Back to Fees page</a></p>";
-                #} 
-				#else 
-				#{
-                    #echo "<h3>Payment Failed</h3>";
-                    #echo "<P>Transaction Status - incomplete</P>";
-                    #echo "<P>Transaction Id - " . $product_transaction . "</p>";
-                    #echo "<p><a href='fees.php'>Back to Fees page</a></p>";
-                #}
-                ?>
+				<section class="receipt">
+				<!--Successful Transaction -->
+					<section class="id pull-left">
+						<p class="what">On-Line-Counselling-Services</p>
+						<p class="what">Customer Receipt</p>
 					</section>
+					<section class="date pull-right">
+						<p class="when">3rd June 2016</p>
+						<p class="when">Receipt: #2132432D</p>
+					</section>
+					<section class="details clearfix">
+						<h1>Receipt</h1>
+						<h2>Service<b class="pull-right">Price</b></h2>
+						<p class="service">Email Session<b class="pull-right gross">£39</b></p>
+						<section class="payment-status clearfix">
+							<h2>Payment status</h2>
+							<p class="text-success service">Successful</p>
+							<!--<p class="text-danger service">Failed</p>-->
+							<h2>Transaction Status</h2>
+							<p class="text-success service">Complete</p>
+							<!--<p class="text-danger service">Failed</p>-->
+							<h2>Transaction ID</h2>
+							<p class="service">5GHTJIDM5$S5</p>
+							<!--<p class="service">N/A</p>-->
+						</section>
+						<section class="cost">
+							<h3>Total: <b class="total">£39.00</b></h3>
+						</section>
+					</section>
+					<button type="button" class="btn btn-success"><a href="fees.php"><i class="fa fa-chevron-left"></i>&nbsp;&nbsp;Fees page</a></button>
+					<button type="button" class="btn btn-info pull-right" onclick="window.print()"><i class="fa fa-print"></i>&nbsp;&nbsp;Print Receipt</button>
+				</section>
+                <?php
+                /* Rechecking the product price and currency detail
+				
+                if ($product_price == $result['product_price'] && $product_currency == $result['product_currency']) 
+				{
+                    echo "<h3>Payment Successful</h3>";
+                    echo "<P>Transaction Status - " . $product_status . "</P>";
+                    echo "<P>Transaction Id - " . $product_transaction . "</P>";
+                    echo "<p><a href='fees.php'>Back to Fees page</a></p>";
+                } 
+				else 
+				{
+                    echo "<h3>Payment Failed</h3>";
+                    echo "<P>Transaction Status - incomplete</P>";
+					echo "<P>Transaction Id - " . $product_transaction . "</p>";
+                    echo "<p><a href='fees.php'>Back to Fees page</a></p>";
+                 }*/
+                ?>
 				</section>
 			</section>
         </section>
