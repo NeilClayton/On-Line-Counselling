324c45f9f3f32e27d4864abdad6a84b5043509b2
Dominic Hart <dom_h@live.co.uk>
2016-07-11 13:53:04 +0100

    Added branding to footer
    -

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,21 @@
 <footer>
-    <p><?php date_default_timezone_set("Europe/London"); echo date('Y') . ' ' . 'On-line-Counselling-Services.'; ?> All rights reserved.</p>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <p><?php date_default_timezone_set("Europe/London"); echo date('Y') . ' ' . 'On-line-Counselling-Services.'; ?> All rights reserved.</p>
+            </div>
+            <div class="col-md-12 no-gutter">
+                <div class="wwb">
+                    <ul>
+                        <li><a href="#">W3C HTML5 <i class="fa fa-check" aria-hidden="true"></i></a></li>
+                        <li><a href="#">W3C CSS3 <i class="fa fa-check" aria-hidden="true"></i></a></li>
+                        <li>Designed and hosted by <a href="http://www.wewontbyte.co.uk">WeWontByte.</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </footer>
 <script src="js/navscroll.js"></script>
 <script src="js/sbar-toggle.js"></script>
